d942f616d64142fb1c973b23ddbc0f9e58627799
marcos ventura <marcosventura.dev@gmail.com>
2023-01-03 10:36:45 -0300

    Conexao com o banco de dados

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,16 +4,32 @@ namespace App\Controllers;
 
 
 use MF\Controller\Action;
+use App\Connection;
+use App\Models\Produto;
 
 class IndexController extends Action{
 
     public function index(){
 
-        $this->view->dados = array('sofá', 'Cadeira', 'Cama');
+       // $this->view->dados = array('sofá', 'Cadeira', 'Cama');
+
+       //instância de conexao;
+        $conn = Connection::getDb();
+
+       //instanciar modelo
+        $produto = new Produto($conn);
+
+        $produtos = $produto->getProdutos();
+
+        $this->view->dados = $produtos;
+
+
+
+        
         $this->render('index','layout1');
     }
     public function sobreNos(){
-        $this->view->dados = array('Notebook', 'Smartphone');
+       // $this->view->dados = array('Notebook', 'Smartphone');
         $this->render('sobreNos', 'layout2');
     }
 
